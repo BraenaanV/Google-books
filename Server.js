3739f660aad396f3fe246d6741b57b3c9c65431f
BraenaanV <braenaan.vaughan@gmail.com>
2021-01-23 14:04:33 -0500

    basic structure, controllers, models server.js done

---
+++ Server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+
+//tell app what to use
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+//setup static assets
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+//connect to Mongo
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongo://localhost/books",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+);
+
+//server start & listen
+
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
